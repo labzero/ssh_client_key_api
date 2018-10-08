b67b1b2a98979bb2a9282f73c70e612bd3ad144d
Brien Wankel <brien@reebosak.net>
2018-10-08 13:57:58 -0700

    Move credo:disable to correct line

--- lib/ssh_client_key_api.ex
+++ lib/ssh_client_key_api.ex
@@ -126,8 +126,8 @@ defmodule SSHClientKeyAPI do
   defp decode_pem_entry({_type, _data, {alg, _}} = entry, phrase) do
     {:ok, :public_key.pem_entry_decode(entry, phrase)}
   rescue
-    # credo:disable-for-next-line Credo.Check.Warning.RaiseInsideRescue
     _e in MatchError ->
+      # credo:disable-for-next-line Credo.Check.Warning.RaiseInsideRescue
       raise KeyError, {:incorrect_passphrase, alg}
   end
 
